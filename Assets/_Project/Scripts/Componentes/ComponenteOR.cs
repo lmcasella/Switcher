79b15d0ffb2ed3f163d1107c23d25f58cc5f9c60
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2024-06-12 17:16:20 -0300

    Componente OR, Actualizcaion Placa, Mecanismos Prefabricados con placas

---
+++ Assets/_Project/Scripts/Componentes/ComponenteOR.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+namespace Componentes {
+    public class ComponenteOR : ComponenteBinario
+    {
+        [SerializeField] private ComponenteBinario A, B;
+
+        private void OnEnable()
+        {
+            if (!(A && B)) return;
+            A.OnEncender += RecibirConexion;
+            A.OnApagar += RecibirConexion;
+            B.OnEncender += RecibirConexion;
+            B.OnApagar += RecibirConexion;
+        }
+
+        private void OnDisable()
+        {
+            A.OnEncender -= RecibirConexion;
+            A.OnApagar -= RecibirConexion;
+            B.OnEncender -= RecibirConexion;
+            B.OnApagar -= RecibirConexion;
+        }
+
+        private void RecibirConexion(object sender, EventArgs eventArgs)
+        {
+            Encender(A.Encendido || B.Encendido);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (!(A && B)) return;
+            MostrarConexionDeInterruptor(A);
+            MostrarConexionDeInterruptor(B);
+        }
+
+        private void MostrarConexionDeInterruptor(ComponenteBinario interruptor)
+        {
+            Gizmos.color = interruptor.Encendido ? Color.green : Color.red;
+            Gizmos.DrawLine(interruptor.transform.position, transform.position);
+        }
+    }
+}

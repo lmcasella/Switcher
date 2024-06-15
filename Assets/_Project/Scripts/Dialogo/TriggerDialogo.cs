194b5ae644819e31b2002d84dca4b215ba001df7
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2024-06-14 23:24:28 -0300

    Prefab trigger de dialogos

--- Assets/_Project/Scripts/Dialogo/TriggerDialogo.cs
+++ Assets/_Project/Scripts/Dialogo/TriggerDialogo.cs
@@ -1,19 +1,23 @@
 using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Dialogo
 {
-    
+    [RequireComponent(typeof(Collider2D))]
     public class TriggerDialogo : MonoBehaviour
     {
         [SerializeField] private string mensaje = "Hola!";
         [SerializeField] private float delay = 3;
         [SerializeField] private bool sacarAlSalir = false;
+        [SerializeField] private bool destruir = false;
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.TryGetComponent(out ControlJugador jugador))
             {
                 jugador.Decir(mensaje, delay);
+                if (destruir)
+                    Destroy(gameObject);
             }
         }
 

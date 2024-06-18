4a0ba3a82460938d6fa2adca637151121aea593c
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2024-06-18 19:09:07 -0300

    Agregado sprite de palanca

--- Assets/_Project/Scripts/Items/Item.cs
+++ Assets/_Project/Scripts/Items/Item.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 public class Item : MonoBehaviour, IUsable
 {
     private ControlJugador _jugador;
+    private Vector2 _lastPosition;
     
     public void Usar(ControlJugador usuario)
     {
@@ -20,13 +21,30 @@ public class Item : MonoBehaviour, IUsable
 
     public void Soltar()
     {
-        transform.DOJump(_jugador.transform.position, 1, 1, 0.5f);
-        _jugador = null;
+        transform.DOJump(_jugador.transform.position, 1, 1, 0.5f).OnComplete(() =>
+        {
+            _lastPosition = transform.position;
+            _jugador = null;
+        });
+    }
+
+    private void Start()
+    {
+        _lastPosition = transform.position;
     }
 
     private void Update()
     {
         if (_jugador)
             transform.position = Vector2.Lerp(transform.position, (Vector2)_jugador.transform.position + new Vector2(0.5f, 0.5f), Time.deltaTime*10);
+        else
+            HoveringEffect();
+    }
+
+    private void HoveringEffect()
+    {
+        float oscillationValue = Mathf.Sin(Time.time) * 0.25f;
+        Vector2 targetPosition = new Vector2(_lastPosition.x, _lastPosition.y + oscillationValue);
+        transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * 10);
     }
 }
